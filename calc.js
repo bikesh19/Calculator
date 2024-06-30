f2bcc18049bad586c9cce1d2be8c0123ef4c8a43
bikesh19 <sahbikesh000@gmail.com>
2024-06-30 21:06:35 +0545

    c

--- calc.js
+++ calc.js
@@ -12,7 +12,7 @@ Array.from(buttons).forEach((button)=>{
       document.querySelector('input').value = string;
     }
     else if(e.target.innerHTML == 'C'){
-        string = string.substring(0,-1)
+        string = string.slice(0,-1)
         document.querySelector('input').value = string;
       }
     else{ 
